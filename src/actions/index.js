f1f98c73acf5ab84f3b2ced8300ae5a141a280d2
Fernando Bueno <fernando_jordi@hotmail.com>
2022-05-18 16:53:01 -0300

    configuração de redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
-// Coloque aqui suas actions
+const FINANCIAL_INFO = 'FINANCIAL_INFO';
+const PERSONAL_INFO = 'PERSONAL_INFO';
+
+export const financialAction = (state) => ({ type: FINANCIAL_INFO, state });
+export const personalAction = (state) => ({ type: PERSONAL_INFO, state });
+
+export default { FINANCIAL_INFO, PERSONAL_INFO };
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import FINANCIAL_INFO from '../actions/index';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const moneyReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case FINANCIAL_INFO:
+    return {
+      ...state, ...action.state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default moneyReducer;
