9db3c2a9f988bbc16070645bdab137b6f67b9018
Fernando Bueno <fernando_jordi@hotmail.com>
2022-05-23 18:03:57 -0300

    adiciona o requisito 7

---
+++ src/components/table.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import connect from 'react-redux';
+
+export class Table extends Component {
+  render() {
+    // const { expenses } = this.props;
+
+    return (
+      <div>
+        <table>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </table>
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({
+//   expenses: state.wallet.expenses,
+// });
+
+export default Table;
