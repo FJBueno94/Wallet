b42acd539dd54297fd82d2d39fad452ab692a5f6
Fernando Bueno <fernando_jordi@hotmail.com>
2022-05-23 13:41:08 -0300

    adiciona correção no requisito 5

--- src/components/form.js
+++ src/components/form.js
@@ -10,6 +10,9 @@ export class Form extends Component {
     this.state = {
       despesas: 0,
       descricao: '',
+      moedas: 'USD',
+      pagamento: 'Dinheiro',
+      categoria: 'Alimentação',
     };
   }
 
@@ -18,10 +21,18 @@ export class Form extends Component {
     financialAction();
   }
 
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     const {
       despesas,
       descricao,
+      moedas,
+      pagamento,
+      categoria,
     } = this.state;
 
     const { currencies } = this.props;
@@ -33,7 +44,9 @@ export class Form extends Component {
             <input
               type="text"
               id="despesas"
+              name="despesas"
               value={ despesas }
+              onChange={ this.handleChange }
               data-testid="value-input"
             />
           </label>
@@ -42,18 +55,24 @@ export class Form extends Component {
             <input
               type="text"
               id="descricao"
+              name="descricao"
               value={ descricao }
+              onChange={ this.handleChange }
               data-testid="description-input"
             />
           </label>
           <label htmlFor="moedas">
             Moeda:
-            <select name="moedas" id="moedas">
+            <select
+              name="moedas"
+              id="moedas"
+              onChange={ this.handleChange }
+              value={ moedas }
+            >
               {
                 currencies.map((moeda) => (
                   <option
                     key={ moeda }
-                    value={ moeda }
                   >
                     { moeda }
                   </option>
@@ -63,20 +82,32 @@ export class Form extends Component {
           </label>
           <label htmlFor="pagamento">
             Método de Pagamento:
-            <select name="pagamento" id="pagamento" data-testid="method-input">
-              <option value="Dinheiro">Dinheiro</option>
-              <option value="Cartão de crédito">Cartão de crédito</option>
-              <option value="Cartão de débito">Cartão de débito</option>
+            <select
+              name="pagamento"
+              id="pagamento"
+              onChange={ this.handleChange }
+              data-testid="method-input"
+              value={ pagamento }
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
             </select>
           </label>
           <label htmlFor="categoria">
             Categoria:
-            <select name="categoria" id="categoria" data-testid="tag-input">
-              <option value="Alimentação">Alimentação</option>
-              <option value="Lazer">Lazer</option>
-              <option value="Trabalho">Trabalho</option>
-              <option value="Transporte">Transporte</option>
-              <option value="Saúde">Saúde</option>
+            <select
+              name="categoria"
+              id="categoria"
+              value={ categoria }
+              onChange={ this.handleChange }
+              data-testid="tag-input"
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
             </select>
           </label>
         </form>
